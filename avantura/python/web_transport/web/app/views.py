4afa0c22a29b6b71fa7d628f6cb22da36e29cc19
a.d.avramenko <shurik@skotch.nirvana>
2020-08-26 17:59:45 +0300

    kakaha

--- avantura/python/web_transport/web/app/views.py
+++ avantura/python/web_transport/web/app/views.py
@@ -2,12 +2,19 @@
 from flask import render_template
 from app import app
 
+class TMP():
+    def __init__(self):
+        self.html_class = 'choice3'
+        self.onclick = ''
+        self.text = 'POPA'
+
+
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'nickname' : 'Burgers'}
     #return render_template("index.html", title = 'Home', user = user)
-    return render_template("sklad.html", title = 'Home', user = user)
+    return render_template("storage.html", menu = [TMP(), TMP(), TMP()])
 
 
 
