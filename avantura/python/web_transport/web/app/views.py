90d5db6d2f72b1b38e14543fc1a7350517963795
a.d.avramenko <shurik@skotch.nirvana>
2020-08-26 15:30:42 +0300

    mv

--- avantura/python/web_transport/web/app/views.py
+++ avantura/python/web_transport/web/app/views.py
@@ -6,6 +6,7 @@ from app import app
 @app.route('/index')
 def index():
     user = {'nickname' : 'Burgers'}
-    return render_template("index.html", title = 'Home', user = user)
+    #return render_template("index.html", title = 'Home', user = user)
+    return render_template("sklad.html", title = 'Home', user = user)
 
 
